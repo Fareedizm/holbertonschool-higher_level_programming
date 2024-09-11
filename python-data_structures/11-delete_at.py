f38c609f19002aacf7bc53e6a60b7736d66ea431
Fareedizm <guliyevfarid@gmail.com>
2024-09-11 11:24:33 +0000

    Data structure

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if 0 <= idx < len(my_list):
-        # Use slicing to create a new list without the item at index idx
+        # Create a new list without the item at index idx
         return my_list[:idx] + my_list[idx + 1:]
-    # If idx is out of range or negative, return the original list
     return my_list
