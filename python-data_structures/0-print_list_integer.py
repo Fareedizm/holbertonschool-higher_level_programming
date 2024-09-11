c27dfb6fee55c165b9a2349aa3431c3877a24b53
Fareedizm <guliyevfarid@gmail.com>
2024-09-11 07:27:07 +0000

    Data structure

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-    def print_list_integer(my_list=[]):
+def print_list_integer(my_list=[]):
    """Prints each integer from the list on a new line."""
-        for number in my_list:
-            print("{:d}".format(number))
+   for number in my_list:
+ print("{:d}".format(number))
  
