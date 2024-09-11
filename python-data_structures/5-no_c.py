1f7d02976ecbae43a5cf9b6f45d87db76e7ae23b
Fareedizm <guliyevfarid@gmail.com>
2024-09-11 11:12:27 +0000

    Data structure

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    # Create a list to store characters that are not 'c' or 'C'
+    def no_c(my_string):
     result_list = []
-     # Iterate through each character in the input string
-    for char in my_string:
-        # Check if the character is not 'c' and not 'C'
-        if char != 'c' and char != 'C':
-            # Append the character to the result list
-            result_list.append(char)
-      # Join the list into a single string and return it
-    return ''.join(result_list)
+     for char in my_string:
+          if char != 'c' and char != 'C':
+                result_list.append(char)
+                return ''.join(result_list)
--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    # Pad tuples with zeros if they have less than 2 elements
-    tuple_a = (tuple_a + (0, 0))[:2]
-    tuple_b = (tuple_b + (0, 0))[:2]
-    
-    # Add corresponding elements of the tuples
-    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    
+    def add_tuple(tuple_a=(), tuple_b=()):
+        # Pad tuples with zeros if they have less than 2 elements
+       tuple_a = (tuple_a + (0, 0))[:2]
+            tuple_b = (tuple_b + (0, 0))[:2]
+        result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return result
