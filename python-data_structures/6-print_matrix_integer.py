0ec5be580f2220fb9dd9e87f00a525b1bd020a60
Fareedizm <guliyevfarid@gmail.com>
2024-09-11 10:42:22 +0000

    Data structure

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     # Iterate through each row in the matrix
     for row in matrix:
         # Format each integer in the row and join them with a space
-        print(' '.join('{:d}'.format(num) for num in row)
+        print(' '.join('{:d}'.format(num) for num in row))
