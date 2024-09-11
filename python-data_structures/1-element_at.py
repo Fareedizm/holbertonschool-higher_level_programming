0e9ba6c82742bd4489ecbc2775f0489701750726
Fareedizm <guliyevfarid@gmail.com>
2024-09-11 07:38:54 +0000

    Data structure

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,14 @@
-#!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx <0 or idx > len.[my_list]:
+    """Retrieve an element from a list by index.
+
+    Args:
+        my_list (list): The list from which to retrieve the element.
+        idx (int): The index of the element to retrieve.
+
+    Returns:
+        The element at index `idx` if valid, otherwise None.
+    """
+    if idx < 0 or idx >= len(my_list):
         return None
-    return my_list[idx]:
+    return my_list[idx]
+
