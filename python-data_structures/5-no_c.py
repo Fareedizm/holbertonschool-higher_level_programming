a1aa855c694d0b40a051939a9d9cee1c004347bc
Fareedizm <guliyevfarid013@gmail.com>
2024-09-12 08:46:44 +0000

    Data structure

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-    def no_c(my_string):
-    result_list = []
-     for char in my_string:
-          if char != 'c' and char != 'C':
-                result_list.append(char)
-                return ''.join(result_list)
+def no_c(my_string):
+    result = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result += char
+    return result
