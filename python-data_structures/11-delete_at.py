4ac1d3751b58763a61e72fd90ef1cc0a2d2f5adf
Fareedizm <guliyevfarid013@gmail.com>
2024-09-12 09:17:50 +0000

    Data structure

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    
     # Check if the index is valid
     if idx < 0 or idx >= len(my_list):
-        return my_list
-    
+        return my_list 
     # Create a new list excluding the item at the specified index
     new_list = my_list
-    new_list.remove(new_list[idx])
-    
+    new_list.remove(new_list[idx]) 
     return new_list
